6113073f206542bb2e23e61ecfd968fbb0b32c01
thwfhk <thwfhk@163.com>
2020-12-16 21:57:54 +0800

    [vsa] add vsa directory

---
+++ src/vsa-based/buf.py
@@ -0,0 +1,16 @@
+[ 'synth-fun',
+  'f',
+  [['x', 'Int']],
+  'Int',
+  [
+    ['Start', 'Int',
+      [ 'x',
+        ('Int', 3),
+        ('Int', 7),
+        ('Int', 10),
+        ['*', 'Start', 'Start'],
+        ['mod', 'Start', 'Start']
+      ]
+    ]
+  ]
+]
