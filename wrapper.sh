cbce1b268468fc7e515082fed498149634ba9172
Luke Yi <lukeyi@pku.edu.cn>
2021-01-05 18:42:31 +0800

    add wrapper.sh

---
+++ wrapper.sh
@@ -0,0 +1,16 @@
+./SyGuS-solver-exe $1 > tmp1 &
+python3 src/final.py $1 > tmp2 &
+while true; do
+    sleep 1
+    if [ -s tmp1 ]; then
+        cat tmp1
+        break
+    fi
+    if [ -s tmp2 ]; then
+        cat tmp2
+        break
+    fi
+done
+kill -- -$$
+# trap - SIGTERM && kill -- -$$
+# trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
