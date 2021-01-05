5c8107bf6dc46f3d897a81741b8154ce86ac4c9d
thwfhk <thwfhk@163.com>
2021-01-05 10:24:26 +0800

    wrapper bug fix

--- src/wrapper.py
+++ src/wrapper.py
@@ -4,23 +4,28 @@ from subprocess import Popen, PIPE
 import sys
 import time
 
-processes = [Popen(['./SyGuS-solver-exe', sys.argv[1]], stdout=PIPE,
-                   bufsize=1, close_fds=True,
-                   universal_newlines=True),
-             Popen(['python3', 'src/final.py', sys.argv[1]], stdout=PIPE,
-                   bufsize=1, close_fds=True,
-                   universal_newlines=True)]
+processes = [Popen(['./SyGuS-solver-mac', sys.argv[1]], stdout=PIPE,
+           bufsize=1, close_fds=True,
+           universal_newlines=True),
+       Popen(['python3', 'src/final.py', sys.argv[1]], stdout=PIPE,
+           bufsize=1, close_fds=True,
+           universal_newlines=True)]
 
 timeout = 0.1
 finish = False
 while not finish:
+  for p in processes:
+    if p.poll() is not None:
+      res = p.stdout.read()
+      p.kill()
+      processes.remove(p)
+      if res == 'No answer.\n':
+        continue
+      print(res, end='')
+      finish = True
+  if finish:
     for p in processes[:]:
-        if p.poll() is not None:
-            print(p.stdout.read(), end='')
-            p.stdout.close()
-            finish = True
-    if finish:
-        for p in processes[:]:
-            p.kill()
-    else:
-        time.sleep(0.5)
+      p.kill()
+  else:
+    select([p.stdout for p in processes], [],[], timeout)[0]
+    # time.sleep(0.5)
